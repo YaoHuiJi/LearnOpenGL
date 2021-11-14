323addd81c05cb0bdfd965cdea5cbffd421e986e
YaoOnMacbook <17535084@qq.com>
2021-11-14 15:46:35 +0800

    add cpp learn target

---
+++ OpenGLPlayground/src/cppMain.cpp
@@ -0,0 +1,15 @@
+//
+//  cppPlayground.cpp
+//  OpenGLPlayground
+//
+//  Created by jackyao on 2021/11/14.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, world!";
+    return 0;
+}
